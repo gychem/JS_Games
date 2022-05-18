49e7df9c673ab34a02442faf9f4ba37a30d22cc3
Gychem Baert <gychem.baert@hotmail.com>
2022-05-18 12:04:33 +0200

    Update styling

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,30 +4,65 @@ const weaponRock = document.getElementById("weaponRock");
 const weaponPaper = document.getElementById("weaponPaper");
 const weaponScissor = document.getElementById("weaponScissor");
 const result = document.getElementById("result");
+const computerChoice = document.getElementById("computerChoice");
+const playAgain = document.getElementById("playAgain");
+let weaponChoice;
+let computerChoiceInput;
 
 weaponRock.addEventListener("click", function() { //Rock
-    chooseWeapon("rock");
+    chooseWeapon("Rock");
 }, false);
 
 weaponPaper.addEventListener("click", function() { //Paper
-    chooseWeapon("paper");
+    chooseWeapon("Paper");
 }, false);
 
 weaponScissor.addEventListener("click", function() { //Scissor
-    chooseWeapon("scissor");
+    chooseWeapon("Scissor");
 }, false);
 
 
-function chooseWeapon(weapon) {
-    let randomWeapon = ['rock', 'paper', 'scissor'][Math.floor(Math.random() * 3)];
-    console.log(randomWeapon)
 
+
+function chooseWeapon(weapon) {
+    let randomWeapon = ['Rock', 'Paper', 'Scissor'][Math.floor(Math.random() * 3)];
+    
+   computerChoice.style.visibility = "visible";
+   weaponChoice = `weapon${weapon}`;
+   computerChoiceInput = `choice${randomWeapon}`;
+
+   weaponRock.disabled = true;
+   weaponPaper.disabled = true;
+   weaponScissor.disabled = true;
+   
     if(weapon == randomWeapon) {
-        result.innerText = `${weapon} vs ${randomWeapon}: Same weapon, play again`;
-    } else if((weapon == "rock" && randomWeapon == "scissor") || (weapon == "scissor" && randomWeapon == "paper") || (weapon == "paper" && randomWeapon == "rock")) {
-        result.innerText = `${weapon} vs ${randomWeapon}: You have won !`;
+        result.innerText = `Same weapon, play again`;
+        document.getElementById(weaponChoice).style.border = "2px solid orange";
+        document.getElementById(computerChoiceInput).style.border = "2px solid orange";
+    } else if((weapon == "Rock" && randomWeapon == "Scissor") || (weapon == "Scissor" && randomWeapon == "Paper") || (weapon == "Paper" && randomWeapon == "Rock")) {
+        document.getElementById(weaponChoice).style.border = "2px solid green";
+        document.getElementById(computerChoiceInput).style.border = "2px solid red";
+        result.innerText = `You have won !`;
     } else {
-        result.innerText = `${weapon} vs ${randomWeapon}: You have lost !`;
+        result.innerText = `You have lost !`;
+        document.getElementById(weaponChoice).style.border = "2px solid red";
+        document.getElementById(computerChoiceInput).style.border = "2px solid green";
     }
 
+    playAgain.style.visibility = "visible";
+
+    
 }
+
+playAgain.addEventListener("click", function() { //Playagain
+    weaponRock.style.border = "2px solid black";
+    weaponPaper.style.border = "2px solid black";
+    weaponScissor.style.border = "2px solid black";
+    document.getElementById(computerChoiceInput).style.border = "2px solid black";
+    playAgain.style.visibility = "hidden";
+    result.innerText = '';
+    computerChoice.style.visibility = "hidden";
+    weaponRock.disabled = false;
+    weaponPaper.disabled = false;
+    weaponScissor.disabled = false;
+}, false);
