894ad8de2345c6cb0df42bd62e4492c76e0b3dad
Gychem Baert <gychem.baert@hotmail.com>
2022-05-18 09:52:20 +0200

    add guessing statements wrong,close,correct

--- guessnumber/script.js
+++ guessnumber/script.js
@@ -1,10 +1,10 @@
 // NICE TO HAVE -> GIVE PLAYER THREE LIVES, each time they are wrong they lose a life
 
-
 let randomNumber = Math.floor(Math.random() * 17);
 console.log(randomNumber)
-
-
+let randomNumberMinus = --randomNumber;
+let randomNumberPlus = ++randomNumber;
+let randomNumberPlus2 = ++randomNumberPlus;
 
 window.guessNumber = guessNumber;
 
@@ -12,12 +12,16 @@ window.guessNumber = guessNumber;
 
 function guessNumber() {
     const guessedNumber = document.querySelector("#guessedNumber").value;
-    console.log(guessedNumber);
+    console.log(randomNumberPlus);
 
     if(randomNumber == guessedNumber) {
-        document.querySelector("#guessResult").innerText = `You have succesfully guessed number ${randomNumber}`;
+        document.querySelector("#guessResult").innerText = `Awesome! Your number ${randomNumber} is correct!`;
+    } else if(guessedNumber == randomNumberMinus || guessedNumber == randomNumberPlus2) {
+        document.querySelector("#guessResult").innerText = `${guessedNumber} is so close, but you just missed it! Are you in a class that started on the thirteenth or what?`;
+
+        
     } else {
-        document.querySelector("#guessResult").innerText = `Wrong number ! Try again`;
+        document.querySelector("#guessResult").innerText = `${guessedNumber} is the wrong number ! Try again`;
     }
 
 }
